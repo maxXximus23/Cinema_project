8320dbeca2bd36c8c48d1fb311b8e18822d0b346
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-02 21:02:01 +0200

    commit with declared path for repositories

--- src/main/java/com/dut/CinemaProject/CinemaProjectApplication.java
+++ src/main/java/com/dut/CinemaProject/CinemaProjectApplication.java
@@ -2,14 +2,10 @@ package com.dut.CinemaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.dut.CinemaProject.dao.repos", "com.dut.CinemaProject.services.services", "com.dut.CinemaProject.api.controllers"})
+@SpringBootApplication(scanBasePackages = {"com.dut.CinemaProject.dao.repos"})
 public class CinemaProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaProjectApplication.class, args);
 	}
-
 }
