1c4101bde16862312ddcee3797608d6b6a3fe229
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-05 14:11:30 +0200

    Added import

--- src/main/java/com/dut/CinemaProject/api/controllers/SessionController.java
+++ src/main/java/com/dut/CinemaProject/api/controllers/SessionController.java
@@ -1,14 +1,12 @@
 package com.dut.CinemaProject.api.controllers;
 
 import com.dut.CinemaProject.dto.Session.SessionDto;
+import com.dut.CinemaProject.dto.Session.SessionTicketsList;
 import com.dut.CinemaProject.services.interfaces.ISessionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
