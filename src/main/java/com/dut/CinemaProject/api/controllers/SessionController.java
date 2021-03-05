db4d43d0aafd67fa9ed8e3ffd1ace8fe6c7dc0c1
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-05 13:46:50 +0200

    Changed response logic

--- src/main/java/com/dut/CinemaProject/api/controllers/SessionController.java
+++ src/main/java/com/dut/CinemaProject/api/controllers/SessionController.java
@@ -4,7 +4,6 @@ import com.dut.CinemaProject.dto.Session.SessionDto;
 import com.dut.CinemaProject.services.interfaces.ISessionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,11 +15,11 @@ import java.util.List;
 @RequestMapping("sessions")
 @AllArgsConstructor
 public class SessionController {
-    private ISessionService sessionService;
+    private final ISessionService sessionService;
 
     @GetMapping("/actual")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<List<SessionDto>> getActual(){
-        return ResponseEntity.ok(sessionService.getActualSessions());
+    public List<SessionDto> getActual(){
+        return sessionService.getActualSessions();
     }
 }
