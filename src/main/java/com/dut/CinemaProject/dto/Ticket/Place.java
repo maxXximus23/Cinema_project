5ad3cfb8aa9b263f08be2e9903991c9f67dd5ff0
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-05 16:55:01 +0200

    Added new class for booked tickets

---
+++ src/main/java/com/dut/CinemaProject/dto/Ticket/Place.java
@@ -0,0 +1,13 @@
+package com.dut.CinemaProject.dto.Ticket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Place {
+    private Integer row;
+    private Integer place;
+}
