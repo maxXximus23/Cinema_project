fe2598fe3dfd4d72e4aaf8f29ef5e43ccebfe2f0
Vitaliy821 <46032498+Vitaliy821@users.noreply.github.com>
2021-03-10 12:43:53 +0200

    Response reworked, code refactored

--- src/main/java/com/dut/CinemaProject/services/services/AccountService.java
+++ src/main/java/com/dut/CinemaProject/services/services/AccountService.java
@@ -2,17 +2,19 @@ package com.dut.CinemaProject.services.services;
 
 import com.dut.CinemaProject.dao.repos.UserRepository;
 import com.dut.CinemaProject.dto.User.UserDto;
-import com.dut.CinemaProject.services.exceptions.ItemNotFoundException;
+import com.dut.CinemaProject.exceptions.ItemNotFoundException;
 import com.dut.CinemaProject.services.interfaces.IAccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AccountService implements IAccountService {
-    @Autowired
     private UserRepository userRepository;
+
     @Override
     public UserDto getUserAccount(Long id) {
-        return new UserDto(userRepository.findById(id).orElseThrow(ItemNotFoundException::new));
+        return new UserDto(userRepository.findById(id).orElseThrow(()-> new ItemNotFoundException("User not found")));
     }
 }
