42b3f84903dc09a3955662785509adbca127b2db
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-15 19:57:11 +0200

    Reworked query for actual sessions

--- src/main/java/com/dut/CinemaProject/dao/repos/SessionRepository.java
+++ src/main/java/com/dut/CinemaProject/dao/repos/SessionRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    @Query("SELECT s FROM Session s WHERE s.date > NOW()")
+    //@Query("SELECT s FROM Session s WHERE s.date > NOW()")
+    @Query(value = "SELECT * FROM (SELECT * FROM sessions WHERE date > NOW() ORDER BY date) ses GROUP BY ses.movie_id",
+            nativeQuery = true)
     List<Session> getActualSessions();
 }
