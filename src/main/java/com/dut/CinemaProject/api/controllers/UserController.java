dfc5f83208c5cde52880d1350ea73953740b0310
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-17 13:16:33 +0200

    Changed map routes and removed MainController

--- src/main/java/com/dut/CinemaProject/api/controllers/UserController.java
+++ src/main/java/com/dut/CinemaProject/api/controllers/UserController.java
@@ -1,19 +1,15 @@
 package com.dut.CinemaProject.api.controllers;
 
-import com.dut.CinemaProject.dto.Ticket.PurchaseTicket;
 import com.dut.CinemaProject.dto.User.ChangePassword;
 import com.dut.CinemaProject.dto.User.UserDto;
-import com.dut.CinemaProject.exceptions.ValidationException;
 import com.dut.CinemaProject.services.interfaces.IAccountService;
 import com.dut.CinemaProject.services.interfaces.IUserService;
 import lombok.AllArgsConstructor;
-import org.aspectj.bridge.Message;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("users")
 @AllArgsConstructor
 public class UserController {
     private final IAccountService accountService;
