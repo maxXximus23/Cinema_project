90550c4e95857afe0c52bec75fbdea422d26436b
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-21 19:20:51 +0200

    ADDED migration

--- src/main/java/com/dut/CinemaProject/dao/domain/JwtBlacklist.java
+++ src/main/java/com/dut/CinemaProject/dao/domain/JwtBlacklist.java
@@ -2,13 +2,11 @@ package com.dut.CinemaProject.dao.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "jwt_blacklist", uniqueConstraints= @UniqueConstraint(columnNames={"token"}))
+@Table(name = "jwt_blacklist")
 @Data
 @EqualsAndHashCode
 public class JwtBlacklist {
