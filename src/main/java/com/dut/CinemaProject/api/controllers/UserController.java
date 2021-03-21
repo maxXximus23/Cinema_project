f046b04f4659ca3af7a3b061f2c5de9951c18bcf
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-21 20:13:18 +0200

    CORS enabled

--- src/main/java/com/dut/CinemaProject/api/controllers/UserController.java
+++ src/main/java/com/dut/CinemaProject/api/controllers/UserController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("users")
 @AllArgsConstructor
--- src/main/java/com/dut/CinemaProject/config/SecurityConfig.java
+++ src/main/java/com/dut/CinemaProject/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
