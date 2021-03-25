ca617d69fd11e836aebbaaf148572085dccce316
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-25 20:42:54 +0200

    Changed way to get unblocked movies

--- src/main/java/com/dut/CinemaProject/dao/repos/MovieRepository.java
+++ src/main/java/com/dut/CinemaProject/dao/repos/MovieRepository.java
@@ -2,12 +2,9 @@ package com.dut.CinemaProject.dao.repos;
 
 import com.dut.CinemaProject.dao.domain.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findMovieByIsBlocked(Boolean isBlocked);
-    @Query(value="SELECT * FROM movies WHERE is_blocked = 0", nativeQuery = true)
-    List<Movie> findAllNotBlocked();
 }
